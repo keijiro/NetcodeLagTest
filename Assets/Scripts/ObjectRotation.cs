c282f9464c875ccfde5eaa53e41c786cd328d1b1
Keijiro Takahashi <keijiro@gmail.com>
2025-11-04 14:39:54 +0900

    Better rotation

--- Assets/Scripts/ObjectRotation.cs
+++ Assets/Scripts/ObjectRotation.cs
@@ -5,13 +5,18 @@ public sealed class ObjectRotation : MonoBehaviour
 {
     [SerializeField] float _rotationSpeed = 10f;
 
-    Vector3 _prevPosition;
+    (Vector3 pos, Vector3 dir) _prev;
 
     void LateUpdate()
     {
-        var delta = transform.position - _prevPosition;
-        var rot = Quaternion.LookRotation(delta.normalized);
-        transform.rotation = ExpTween.Step(transform.rotation, rot, _rotationSpeed);
-        _prevPosition = transform.position;
+        var dir = (transform.position - _prev.pos).normalized;
+
+        if (Vector3.Dot(dir, _prev.dir) > 0)
+        {
+            var rot = Quaternion.LookRotation(dir);
+            transform.rotation = ExpTween.Step(transform.rotation, rot, _rotationSpeed);
+        }
+
+        _prev = (transform.position, dir);
     }
 }
